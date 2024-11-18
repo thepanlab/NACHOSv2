66ebf87dc7e184e38a1688a96da22953011fe62a
Paul Calle <pcallec@ou.edu>
2024-11-18 17:14:13 -0600

    Replace reading of metadata files in csv

--- nachosv2/data_processing/read_metadata_csv.py
+++ nachosv2/data_processing/read_metadata_csv.py
@@ -1,7 +1,41 @@
 import os
-import pandas as pd
 import sys
+import pandas as pd
 from termcolor import colored
+from pathlib import Path
+
+def read_metadata_csv(path_label_csv):
+    """
+    Reads all CSV files in the given directory and returns a dictionary of the datas.
+    
+    Args:
+        dierctory_path (str): The directory where are the CSV files.
+        configuration_file (JSON): The configuration file.
+    
+    Returns:
+        data_dict (dict): A dictionary of of the datas from the CSV files.
+                        data_dict = {'subject1': {'image_path': [image_path1, image_path2, ...],
+                                                  'indexes': [0, 1, 2, ...],
+                                                  'label1_number': [...],
+                                                  'label2_number': [...],
+                                                  ...,
+                                                  },
+                                    'subject2': {...},
+                                    ...
+                                    }
+    """
+    
+    # Initializations
+    p = Path(path_label_csv)
+    
+    if not p.is_file():
+        raise FileNotFoundError(f"Error: The file '{p.name}' does not exist in the directory '{p.parent}'.")
+    
+    df_labels = pd.read_csv(p)
+    
+    # Checks if the given path is valid
+
+    return df_labels
 
 
 def read_data_csv(directory_path, configuration_file):
@@ -28,7 +62,6 @@ def read_data_csv(directory_path, configuration_file):
     # Initializations
     data_dict = {}   # The dictionary where the data will be put
     
-    
     # Checks if the given path is valid
     try:
         assert os.path.isdir(directory_path), f"Error: '{directory_path}' is not a valid input path."
@@ -36,14 +69,12 @@ def read_data_csv(directory_path, configuration_file):
     except AssertionError as error:
         print(colored(error, 'red'))
         sys.exit()
-        
-        
-    # Creates the list to read, containing only one time each used subject
-    csv_file_to_read_list = list(set(configuration_file['subject_list']
-                                     + configuration_file['test_subjects']
-                                     + configuration_file['validation_subjects']))
 
-    
+    # Creates the list to read, containing only one time each used subject
+    csv_file_to_read_list = set(configuration_file['subject_list']
+                                + configuration_file['test_subjects']
+                                + configuration_file['validation_subjects'])
+
     # Loops on each CSV file to read it
     for file_to_read in csv_file_to_read_list:
         
@@ -58,16 +89,12 @@ def read_data_csv(directory_path, configuration_file):
             with open(file_path, 'r') as f:
 
                 data_dict[file_to_read] = _loop_read_data_csv(file_path)
-
-                
+            
         except FileNotFoundError:
             print(colored(f"Error: The file '{file_name}' does not exist in the directory '{directory_path}'.", 'red'))
-            sys.exit()
-    
+            sys.exit()\
     
     return data_dict
-
-
 
 def _loop_read_data_csv(file_path):
     """
@@ -80,11 +107,9 @@ def _loop_read_data_csv(file_path):
     # Initialization
     subject_data_dict = {}
     
-    
     # Reads the file and puts it into a panda dataframe
     file_dataframe = pd.read_csv(file_path)
 
-    
     # Saves the image names (first column of hte CSV) into a dictionary
     if not file_dataframe.empty:  # Checks the dataframe is not empty
         
@@ -94,15 +119,11 @@ def _loop_read_data_csv(file_path):
         # Adds the list to the dictionary
         subject_data_dict['image_path'] = image_name_list
 
-    
     # Adds the indexes to the dataframe
     subject_data_dict['indexes'] = file_dataframe.index.tolist()
     
-    
     # Adds the data from the dataframe to the dictionary
     for column in file_dataframe.columns[1:]: # Except the first column
-
         subject_data_dict[column] = file_dataframe[column].tolist()
-    
     
     return subject_data_dict
