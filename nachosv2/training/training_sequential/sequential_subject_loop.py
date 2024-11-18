169e736bfd09a254df4ff6bbf330a5a3bfd332f8
Paul Calle <pcallec@ou.edu>
2024-11-18 17:26:04 -0600

    Add type hint and modifications in sequential_subject_loop

--- nachosv2/training/training_sequential/sequential_subject_loop.py
+++ nachosv2/training/training_sequential/sequential_subject_loop.py
@@ -1,47 +1,64 @@
-from termcolor import colored
+from typing import Dict, Optional, Callable
 
-from nachosv2.training.training_processing.training_variables import TrainingVariables
+from termcolor import colored
+import pandas as pd
+
+from nachosv2.training.training_processing.partitions import generate_list_subjects_for_partitions
 from nachosv2.training.training_processing.training_loop import training_loop
 
 
-def sequential_subject_loop(execution_device, current_configuration, test_subject_name, data_dictionary, number_of_epochs, normalize_transform, is_outer_loop, is_3d = False, is_verbose_on = False):
+def sequential_subject_loop(
+    execution_device: str,
+    current_configuration: Dict,
+    test_subject: str,
+    df_metadata: pd.DataFrame,
+    number_of_epochs: int,
+    do_normalize_2d: bool=False,
+    is_outer_loop: bool=False,
+    is_3d: bool = False,
+    is_verbose_on: bool = False
+):
     """
     Executes the training loop for the given test subject.
 
     Args:
-        execution_device (str): The name of the device that will be use.
+        execution_device (str): The name of the device to be used for training.
         current_configuration (dict): The training configuration.
-        
-        test_subject_name (str): The test subject name.
-        data_dictionary (dict of list): The data dictionary.
-        number_of_epochs (int): The number of epochs.
-        
-        is_outer_loop (bool): If this is of the outer loop.
-        is_verbose_on (bool): If the verbose mode is activated. Default is false. (Optional)
+        test_subject_name (str): The name of the test subject.
+        df_metadata (pd.DataFrame): Dataframe containing filename, subject, and other metadata.
+        number_of_epochs (int): The number of epochs for training.
+        normalize_transform (bool): Whether to normalize the data.
+        is_outer_loop (bool): Whether this is an outer loop run (no validation subjects).
+        is_3d (bool): Whether the data is 3D. Default is False.
+        is_verbose_on (bool): Whether verbose mode is activated. Default is False.
     """
     
     print(colored(
-        f"\n========== " + 
-        f"Starting training for {test_subject_name} in {current_configuration['selected_model_name']}" +
-        f" ==========\n",
+        "\n========== " + 
+        f"Starting training for {test_subject} in {current_configuration['selected_model_name']}" +
+        " ==========\n",
         'green'))
-    
-    
+
     # Gets the needed data for training
-    training_variables = TrainingVariables(current_configuration, test_subject_name, is_outer_loop, is_verbose_on)
     
+    list_subjects_for_partitions = generate_list_subjects_for_partitions(
+        list_validation_subjects=current_configuration.get('validation_subjects', None),
+        list_subjects=current_configuration['subject_list'],
+        test_subject=test_subject,
+        do_shuffle=current_configuration.get('shuffle_the_folds', False)
+        )    
 
     # Starts the training loop
+    
     training_loop(
-        execution_device,                   # The name of the device that will be use
-        current_configuration,              # The input configuration
-        test_subject_name,                  # The name of the testing subject
-        training_variables.fold_list,       # A list of fold partitions
-        training_variables.number_of_folds, # the number of rotations to perform
-        data_dictionary,                    # The data dictionary
-        number_of_epochs,                   # The number of epochs
-        normalize_transform,
-        is_outer_loop,                      # If this is of the outer loop
-        is_3d,
-        is_verbose_on = is_verbose_on       # If the verbose mode is activated
+        execution_device=execution_device,
+        configuration=current_configuration,
+        test_subject=test_subject,
+        list_subjects_for_partitions=list_subjects_for_partitions,
+        df_metadata=df_metadata,
+        number_of_epochs=number_of_epochs,
+        do_normalize_2d=do_normalize_2d,
+        is_outer_loop=is_outer_loop,
+        is_3d=is_3d,
+        is_verbose_on=is_verbose_on
     )
