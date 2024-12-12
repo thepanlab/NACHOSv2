8f072614fab16a0b4eadad0c901642caf6c54f79
Paul Calle <pcallec@ou.edu>
2024-12-12 12:00:35 -0600

    Update training_loop.py from subjects to folds

--- nachosv2/training/training_processing/training_loop.py
+++ nachosv2/training/training_processing/training_loop.py
@@ -5,16 +5,18 @@ from termcolor import colored
 from nachosv2.training.training_processing.training_fold import TrainingFold
 from nachosv2.checkpoint_processing.read_log import read_item_list_in_log
 from nachosv2.checkpoint_processing.load_save_metadata_checkpoint import write_log
-from nachosv2.training.training_processing.partitions import generate_list_subjects_for_partitions
+from nachosv2.training.training_processing.partitions import generate_list_folds_for_partitions
+
 
 def training_loop(
     execution_device: str,
     configuration: Dict,
-    test_subject: str,
+    test_fold_name: str,
     df_metadata: pd.DataFrame,
     number_of_epochs: int,
     do_normalize_2d: bool,
     is_outer_loop: bool,
+    use_mixed_precision: bool=False,
     is_3d: bool = False,
     rank: Optional[int] = None,
     is_verbose_on: bool = False
@@ -25,9 +27,8 @@ def training_loop(
     Args:
         execution_device (str): The device to be used for training.
         configuration (dict): The input configuration.
-        test_subject (str): The name of the testing subject.
-        list_subjects_for_partitions (list of dict): A list of partitions.
-        data_dictionary (dict of list): The data dictionary.
+        test_fold_name (str): The name of the test fold.
+        df_metadata (pandas datafram): data frame containing the metadata (filepath, label, fold, etc)
         number_of_epochs (int): The number of epochs.
         normalize_transform (bool): Whether to normalize the data.
         is_outer_loop (bool): Indicates if this is the outer loop.
@@ -36,7 +37,7 @@ def training_loop(
         is_verbose_on (bool): Indicates if verbose mode is activated. Default is False.
     """
     
-    print(colored(f'Beginning the training loop for {test_subject}.', 'green'))
+    print(colored(f'Beginning the training loop for {test_fold_name}.', 'green'))
     
     
     # Gets the current rotation progress
@@ -57,40 +58,41 @@ def training_loop(
     
     # Trains for every current_rotation specified
     
-    list_subjects_for_partitions = generate_list_subjects_for_partitions(
-        list_validation_subjects=configuration.get('validation_subjects', None),
-        list_subjects=configuration['subject_list'],
-        test_subject=test_subject,
+    folds_for_partitions_list = generate_list_folds_for_partitions(
+        validation_fold_list=configuration.get('validation_fold_list', None),
+        fold_list=configuration['fold_list'],
+        test_fold_name=test_fold_name,
         do_shuffle=configuration.get('shuffle_the_folds', False)
         ) 
     
-    number_of_rotations = len(list_subjects_for_partitions)
+    number_of_rotations = len(folds_for_partitions_list)
     
     for rotation in range(current_rotation, number_of_rotations):
         
         # Outer loop
         if is_outer_loop:
-            validation_subject = None
-            print(colored(f'--- Rotation {rotation + 1}/{number_of_rotations} for test subject {test_subject} ---', 'magenta'))
+            validation_fold_name = None
+            print(colored(f'--- Rotation {rotation + 1}/{number_of_rotations} for test subject {test_fold_name} ---', 'magenta'))
         
         # Inner loop will use the validation subjects
         else:
-            validation_subject = list_subjects_for_partitions[rotation]['validation'][0]
-            print(colored(f'--- Rotation {rotation + 1}/{number_of_rotations} for test subject {test_subject} and val subject {validation_subject} ---', 'magenta'))
+            validation_fold_name = folds_for_partitions_list[rotation]['validation'][0]
+            print(colored(f'--- Rotation {rotation + 1}/{number_of_rotations} for test subject {test_fold_name} and val subject {validation_fold_name} ---', 'magenta'))
         
-        list_training_subjects = list_subjects_for_partitions[rotation]['training']
+        training_folds_list = folds_for_partitions_list[rotation]['training']
         
         # Creates and runs the training fold for this subject pair
         training_fold = TrainingFold(
             execution_device,        # The name of the device that will be use
             rotation,                # The fold index within the loop
             configuration,           # The training configuration
-            test_subject,            # The test subject name
-            validation_subject,      # The validation_subject name
-            list_training_subjects,  # A list of fold partitions
+            test_fold_name,            # The test subject name
+            validation_fold_name,      # The validation_subject name
+            training_folds_list,  # A list of fold partitions
             df_metadata,             # The data dictionary
             number_of_epochs,        # The number of epochs
             do_normalize_2d,         # 
+            use_mixed_precision,
             rank,                    # An optional value of some MPI rank. Default is none. (Optional)
             is_outer_loop,           # If this is of the outer loop. Default is false. (Optional)
             is_3d,                   # 
@@ -102,7 +104,7 @@ def training_loop(
         # Writes the index to log
         write_log(
             config = configuration,
-            testing_subject = testing_subject,
+            test_fold_name = test_fold_name,
             rotation = rotation,
             log_directory = configuration['output_path'],
             log_rotations = None,
