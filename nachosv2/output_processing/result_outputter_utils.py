5f3b02dd53cb78f79ab84629bf5d95be7715f9be
Paul Calle <pcallec@ou.edu>
2025-01-14 18:17:11 -0600

    Add type hint and fix function to store results

--- nachosv2/output_processing/result_outputter_utils.py
+++ nachosv2/output_processing/result_outputter_utils.py
@@ -3,39 +3,58 @@ import csv
 import os
 import pandas as pd
 from termcolor import colored
+from pathlib import Path
+from typing import Dict, List, Optional
+
+from torch import nn
 
 from nachosv2.model_processing.predict_model import predict_model
 
 
-def create_folders(path, names = None):
+def create_folders(path: Path,
+                   names: Optional[List[str]] = None):
     """
     Creates folder(s) if they do not exist.
 
     Args:
-        path (str): Name of the path to the folder.
+        path (Path): Path to the folder.
         names (list): The folder name(s). (Optional)
     """
     
     if names is not None:
-        
         for name in names:
-            
-            folder_path = os.path.join(path, name)
-            if not os.path.exists(folder_path): os.makedirs(folder_path)
-            
+            folder_path = path / name
+            if not folder_path.exists():
+                folder_path.mkdir(mode=0o777, parents=True, exist_ok=True)
     else:
-        if not os.path.exists(path): os.makedirs(path)
+        if not path.exists():
+            path.mkdir(mode=0o777, parents=True, exist_ok=True)
 
 
-
-def save_history(history, path_prefix, file_prefix):
+def save_history(history: Dict[str, List[float]],
+                 path_prefix: Path,
+                 file_prefix: str):
     if history is not None:
-        history = pd.DataFrame(history)
-        history.to_csv(f"{path_prefix}/{file_prefix}_history.csv")
+        history_df = pd.DataFrame(history)
+        history_df.to_csv(path_prefix / f"{file_prefix}_history.csv")
     
     else:
         print(colored(f'Warning: The model history is empty for: {file_prefix}', 'yellow'))
 
+
+def metric_writer2(values: List[Dict],
+                   path: Path,
+                   filename: str):
+    """
+    Writes some list in a file.
+
+    Args:
+        path (str): A file path.
+        values (list): A list of values.
+        path_prefix (str): The prefix of the file name and directory.
+    """
+    values_df = pd.DataFrame(values)   
+    values_df.to_csv(path / filename)
 
 
 def metric_writer(path, values, path_prefix):
@@ -96,10 +115,13 @@ def save_outer_loop(execution_device, trained_model, datasets, metrics, file_pre
     
     # Saves file names for the testing dataset
     metrics[f'file_name/{file_prefix}_file.csv'] = file_names_list
+       
     
-    
-    
-def save_inner_loop(execution_device, trained_model, datasets, metrics, file_prefix):
+def save_inner_loop(execution_device,
+                    trained_model,
+                    datasets,
+                    metrics,
+                    file_prefix):
     """
     Saves the results of the inner loop to the metrics dictionary.
 
@@ -113,6 +135,42 @@ def save_inner_loop(execution_device, trained_model, datasets, metrics, file_pre
     
     # Predicts probability results for the testing and validation datasets
     predicted_labels_in_testing, predicted_probabilities_in_testing, true_labels_list_in_testing, file_names_list_in_testing = predict_model(execution_device, trained_model, datasets['testing']['ds'])
+    predicted_labels_in_validation, predicted_probabilities_in_validation, true_labels_list_in_validation, file_names_list_in_validation = predict_model(execution_device, trained_model, datasets['validation']['ds'])
+    
+    
+    # Saves predicted probabilities for the testing and validation datasets
+    metrics[f"prediction/{file_prefix}_test_predicted_labels.csv"] = [l for l in predicted_labels_in_testing]
+    metrics[f"prediction/{file_prefix}_test_predicted_probabilities.csv"] = [l for l in predicted_probabilities_in_testing]
+    metrics[f"prediction/{file_prefix}_val_predicted_labels.csv"] = [l for l in predicted_labels_in_validation]
+    metrics[f"prediction/{file_prefix}_val_predicted_probabilities.csv"] = [l for l in predicted_probabilities_in_validation]
+    
+    # Saves true labels for the testing and validation datasets
+    metrics[f"true_label/{file_prefix}_test_true_label.csv"] = true_labels_list_in_testing
+    metrics[f"true_label/{file_prefix}_val_true_label.csv"] = true_labels_list_in_validation
+    
+    # Saves file names for the testing and validation datasets
+    metrics[f'file_name/{file_prefix}_test_file.csv'] = file_names_list_in_testing
+    metrics[f'file_name/{file_prefix}_val_file.csv'] =  file_names_list_in_validation
+
+
+def generate_inner_loop_results(execution_device:str,
+                                trained_model: nn.Module,
+                                datasets: dict,
+                                metrics: dict,
+                                file_prefix: str):
+    """
+    Saves the results of the inner loop to the metrics dictionary.
+
+    Args:
+        execution_device (str): The device on which the model is executed.
+        trained_model (nn.Module): The trained model to be used for predictions.
+        datasets (dict): Dictionary containing 'testing' and 'validation' datasets.
+        metrics (dict): Dictionary to store the metrics and results.
+        file_prefix (str): Prefix for the file names used in saving the results.
+    """
+    
+    # Predicts probability results for the testing and validation datasets
+    # predicted_labels_in_testing, predicted_probabilities_in_testing, true_labels_list_in_testing, file_names_list_in_testing = predict_model(execution_device, trained_model, datasets['testing']['ds'])
     predicted_labels_in_validation, predicted_probabilities_in_validation, true_labels_list_in_validation, file_names_list_in_validation = predict_model(execution_device, trained_model, datasets['validation']['ds'])
     
     
