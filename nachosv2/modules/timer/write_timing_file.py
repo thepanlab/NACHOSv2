91694b35612721e8c54b4b659baa583e3c5c457f
Paul Calle <pcallec@ou.edu>
2025-02-25 11:12:20 -0600

    Change filename for time

--- nachosv2/modules/timer/write_timing_file.py
+++ nachosv2/modules/timer/write_timing_file.py
@@ -1,10 +1,10 @@
-import os
+from pathlib import Path
 from termcolor import colored
-
 from nachosv2.modules.timer.precision_timer import PrecisionTimer
 
 def write_timing_file(timer: PrecisionTimer,
-                      directory_path: str,
+                      directory_path: Path,
+                      config_dict: dict,
                       is_verbose_on: bool = False):
     """
     Creates a timing file and writes in it the elapsed time.
@@ -23,10 +23,9 @@ def write_timing_file(timer: PrecisionTimer,
         elapsed_time = timer.get_elapsed_time()
         timer_name = timer.get_timer_name()
         
-        os.makedirs(directory_path, exist_ok=True)
-
-        timing_file = f"_TIME_SEQ_INNER_{timer_name}.txt"
-        timing_path = os.path.join(directory_path, timing_file)
+        timing_file = f"{config_dict['job_name']}"+ \
+                      f"_TIME_SEQ_INNER_{timer_name}.txt"
+        timing_path = directory_path / timing_file
 
         with open(timing_path, 'w') as file_pointer:
             file_pointer.write(str(elapsed_time))
