cad6892e90e9db4c96dcfe18b36135cfa6098b29
Paul Calle <pcallec@ou.edu>
2025-02-25 11:12:56 -0600

    updating training_sequential

--- nachosv2/training/training_sequential/training_sequential.py
+++ nachosv2/training/training_sequential/training_sequential.py
@@ -1,14 +1,15 @@
 import os
 import sys
+from pathlib import Path
 from termcolor import colored
 
-from nachosv2.training.training_sequential.sequential_processing import sequential_processing
+from nachosv2.training.training_sequential.execute_training import execute_training
 from nachosv2.modules.timer.precision_timer import PrecisionTimer
 from nachosv2.modules.timer.write_timing_file import write_timing_file
 from nachosv2.output_processing.memory_leak_check import initiate_memory_leak_check, end_memory_leak_check
 from nachosv2.setup.command_line_parser import parse_command_line_args
 from nachosv2.setup.define_execution_device import define_execution_device
-from nachosv2.setup.get_config_list import get_config_list
+from nachosv2.setup.get_config import get_config
 
 
 """
@@ -25,7 +26,7 @@ def run_training():
     args = parse_command_line_args()
     
     # Defines the arguments
-    list_dict_configs = get_config_list(args['file'], args['folder'])
+    config_dict = get_config(args['file'])
     
     is_verbose_on = args['verbose']
     
@@ -53,28 +54,30 @@ def run_training():
     else:
         is_cv_loop = True
         
-    sequential_processing(
+    execute_training(
         execution_device,
-        list_dict_configs,
+        config_dict,
         is_cv_loop,
         is_verbose_on
         )
 
     # Checks for memory leaks
     if memory_leak_check_enabled:
-        end_memory_leak_check(memory_snapshot)   
+        end_memory_leak_check(memory_snapshot)
     
     # Stops the timer and prints the elapsed time
     elapsed_time_seconds = training_timer.get_elapsed_time()
-    print(colored(f"\nElapsed time: {elapsed_time_seconds} seconds.", 'magenta'))
+    print(colored(f"\nElapsed time: {elapsed_time_seconds:.2f} seconds.", 'magenta'))
 
     # Creates a file and writes elapsed time in it
     # Make the next line fit in 80 characters
-    timing_directory_path = "../results/training_timings" # The directory's path where to put the timing file
+    timing_directory_path = Path(config_dict["output_path"]) / "training_timings" # The directory's path where to put the timing file
+    timing_directory_path.mkdir(mode=0o777, parents=True, exist_ok=True)
+    
     write_timing_file(training_timer,
                       timing_directory_path,
+                      config_dict,
                       is_verbose_on)
-
 
 # Sequential Inner Loop
 if __name__ == "__main__":
