1fa26a257df3958187b00999178add95856f3428
Paul Calle <pcallec@ou.edu>
2025-02-28 22:27:42 -0600

    remove unnecessary parts and use ensure_path_exists

--- nachosv2/results_processing/confusion_matrix/get_confusion_matrix.py
+++ nachosv2/results_processing/confusion_matrix/get_confusion_matrix.py
@@ -11,6 +11,8 @@ from nachosv2.setup.utils import get_filepath_list
 from nachosv2.setup.utils import determine_if_cv_loop
 from nachosv2.setup.utils import get_default_folder
 from nachosv2.setup.utils import get_newfilepath_from_predictions
+from nachosv2.setup.files_check import ensure_path_exists
+
 
 def generate_individual_confusion_matrix(path: Path) -> pd.DataFrame:
     
@@ -50,32 +52,10 @@ def generate_cf(results_path: Path,
     
     if is_cv_loop is None:
         is_cv_loop = determine_if_cv_loop(predictions_file_path_list[0])
-    
-    if is_cv_loop:
-        # Regex to match test and validation info
-        # cross-validation
-        pattern = r"test_([A-Za-z0-9]+)_hpconfig_([0-9]+)_val_([A-Za-z0-9]+)"
-    # cross-validation
-    else:
-        pattern = r"test_([A-Za-z0-9]+)_hpconfig_([0-9]+)"
-    
+        
     # Extract and print test and validation fold numbers
     for predictions_path in predictions_file_path_list:
-        filename = predictions_path.name
-        match = re.search(pattern, filename)
-        if match:
-            groups = match.groups()
-            test_fold = groups[0]
-            hp_config = groups[1]
-            val_fold = groups[2] if len(groups) > 2 else None  # Conditional assignment for val_fold
-            
-            print(f"File: {filename}")
-            print(f"Test fold: {test_fold}")
-            print(f"Hyperparameter configuration index: {hp_config}")
-            if val_fold:  # Print validation fold only if it's available
-                print(f"Validation fold: {val_fold}")
-            print("-----")
-            
+        filename = predictions_path.name            
         cf_df = generate_individual_confusion_matrix(predictions_path)
 
         cf_filepath = get_newfilepath_from_predictions(predictions_path,
@@ -90,14 +70,8 @@ def main():
     The Main Program.
     """
     args = parse_command_line_args()
-    # Gets program configuration and run using its contents
-    # config_dict_list is a list of dictionaries
     config_dict = get_config(args['file'])
-
-    # Look for csv files with specific format
-    if not Path(config_dict['results_path']).exists():
-        raise FileNotFoundError(print(colored(f"Path {config_dict['results_path']} does not exist.", "red")))
-
+    ensure_path_exists(config_dict['results_path'])
     results_path = Path(config_dict['results_path'])
     
     output_path = config_dict.get('output_path', None)
