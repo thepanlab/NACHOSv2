8a5a8ff6596272da09090fd7af3e080a86cfd0e7
Paul Calle <pcallec@ou.edu>
2025-02-28 21:52:53 -0600

    get epoch analysis

--- nachosv2/results_processing/epoch_counting/epoch_counting.py
+++ nachosv2/results_processing/epoch_counting/epoch_counting.py
@@ -1,10 +1,15 @@
-from src.results_processing.results_processing_utils.get_configuration_file import parse_json
-from termcolor import colored
-from src.file_processing import path_getter
-import pandas as pd
-import math
+from pathlib import Path
+import re
+from typing import Optional
 import os
-
+import math
+from termcolor import colored
+import pandas as pd
+from nachosv2.setup.command_line_parser import parse_command_line_args
+from nachosv2.setup.get_config import get_config
+from nachosv2.setup.utils import get_filepath_list
+from nachosv2.setup.utils import determine_if_cv_loop
+from nachosv2.setup.utils import get_filepath_from_results_path
 
 def count_epochs(history_paths, is_outer):
     """
@@ -169,37 +174,119 @@ def save_epoch_avg_stderr(epochs, output_path, config_nums):
     print(colored('Successfully printed epoch averages/stderrs to: ' + file_name, 'green'))
 
 
+def fill_dataframe(filepath: Path,
+                   df: pd.DataFrame,
+                   is_cv_loop: bool):
+    
+    # Read CSV file into DataFrame
+    history_df = pd.read_csv(filepath)
+
+    # Example: Extracting the epoch with the lowest loss (assuming a column 'loss' exists)
+    # You need to adjust this based on your actual CSV column names and what you define as 'best'
+    if 'validation_loss' in history_df.columns:
+        # Logic: it identifies the minimum value for 'validation loss'
+        best_epoch_index = history_df['validation_loss'].idxmin()
+        best_epoch_data = history_df.iloc[best_epoch_index]
+
+         # Extract information from the file name
+        parts = filepath.stem.split('_')
+        test_fold = parts[1]  # Adjust based on actual index in your filenames
+        hp_config = parts[3]  # Adjust based on actual index in your filenames
+        val_fold = parts[5] if is_cv_loop else None  # Check if it's a CV loop
+
+        # Prepare row as a DataFrame to be concatenated
+        new_row_df = pd.DataFrame({
+            'test_fold': [test_fold],
+            'hp_config': [hp_config],
+            'val_fold': [val_fold],
+            'best_epoch': [best_epoch_index],
+            'best_loss': [best_epoch_data['train_loss']]
+        })
+
+        # Concatenate the new row to the existing DataFrame
+        df = pd.concat([df, new_row_df], ignore_index=True)
+
+    return df
+    
+
+def generate_summary_epochs(results_path: Path,
+                            output_path: Optional[Path],
+                            is_cv_loop: Optional[bool]=None):
+    
+    suffix_filename = "history"
+    history_filepath_list = get_filepath_list(results_path,
+                                              suffix_filename)
+    
+    if is_cv_loop is None:
+        is_cv_loop = determine_if_cv_loop(history_filepath_list[0])
+    
+    if is_cv_loop:
+        # Regex to match test and validation info
+        # cross-validation
+        pattern = r"test_([A-Za-z0-9]+)_hpconfig_([0-9]+)_val_([A-Za-z0-9]+)"
+    # cross-validation
+    else:
+        pattern = r"test_([A-Za-z0-9]+)_hpconfig_([0-9]+)"
+
+    df_epochs = pd.DataFrame()
+    
+    # Extract and print test and validation fold numbers
+    for history_path in history_filepath_list:
+        filename = history_path.name
+        match = re.search(pattern, filename)
+        if match:
+            groups = match.groups()
+            test_fold = groups[0]
+            hp_config = groups[1]
+            val_fold = groups[2] if len(groups) > 2 else None  # Conditional assignment for val_fold
+            
+            print(f"File: {filename}")
+            print(f"Test fold: {test_fold}")
+            print(f"Hyperparameter configuration index: {hp_config}")
+            if val_fold:  # Print validation fold only if it's available
+                print(f"Validation fold: {val_fold}")
+            print("-----")
+            
+        df_epochs = fill_dataframe(history_path, df_epochs, is_cv_loop)
+
+        print("hello")
+    
+    epochs_filepath = get_filepath_from_results_path(results_path,
+                                                    "epochs_analysis",
+                                                    "epochs_results_all.csv",
+                                                     output_path)
+    
+    df_epochs.to_csv(epochs_filepath)
+
 
 def main(config = None):
     """
     The main body of the program
     """
-    
+    # TODO
+    # modify to new values
     # Obtain a dictionary of configurations
-    if config is None:
-        config = parse_json('./results_processing/epoch_counting/epoch_counting_config.json')
-        
-    if not os.path.exists(config['output_path']):
-        os.makedirs(config['output_path'])
-
-    # Get the necessary input files
-    history_paths = path_getter.get_history_paths(config['data_path'])
-    is_outer = config['is_outer']
-
-    # Count the number of epochs within every file
-    epochs = count_epochs(history_paths, is_outer)
-
-    # Get config nums (E.g config 1)
-    config_nums = path_getter.get_config_indexes(config['data_path'])
-
-    # Output the counts
-    save_epoch_counts(epochs, config['output_path'], config_nums, is_outer)
-
-    # Output the stderr
-    if not is_outer:
-        save_epoch_avg_stderr(epochs, config['output_path'], config_nums)
-
-
+    
+        # Parses the command line arguments
+    args = parse_command_line_args()
+    
+    # Defines the arguments
+    config_dict = get_config(args['file'])
+    
+    if not Path(config_dict['results_path']).exists():
+        raise FileNotFoundError(print(colored(f"Path {config_dict['results_path']} does not exist.", "red")))
+    
+    results_path = Path(config_dict['results_path'])
+    
+    output_path = config_dict.get('output_path', None)
+    if output_path is not None:
+        output_path = Path(output_path)
+    
+    is_cv_loop = config_dict.get('is_cv_loop', None)
+    
+    generate_summary_epochs(results_path, output_path,
+                            is_cv_loop)
+    
 
 if __name__ == "__main__":
     
---
+++ nachosv2/setup/utils.py
@@ -0,0 +1,123 @@
+from pathlib import Path
+from typing import Optional, List
+from termcolor import colored
+
+
+def get_filepath_list(directory_to_search_path: Path,
+                      string_in_filename: str) -> List[Path]:
+    """
+    Recursively searches for CSV files containing 'string_in_filename'
+    in their filenames within a given directory.
+
+    Args:
+    directory (Path): The path to the directory where the search will start.
+
+    Returns:
+    List[Path]: A list of Paths to the matching CSV files.
+    """
+    # Check if the given directory is indeed a directory
+    if not directory_to_search_path.is_dir():
+        raise ValueError("The provided path is not a directory.")
+    
+    # List to store the paths of matching CSV files
+    matched_files = []
+    
+    # Using rglob to recursively search for files
+    for file in directory_to_search_path.rglob(f'*{string_in_filename}*.csv'):
+        matched_files.append(file)
+    
+    return matched_files
+
+
+def determine_if_cv_loop(path: Path):
+    """Based on the filepath name of results we can determine if it is a cross-validation loop or cross-testing loop
+
+    Args:
+        path (Path): any results filepath
+
+    Returns:
+        bool: if it is cv loop
+    """
+    if "_val_" in path.name:
+        return True
+    else:
+        return False
+    
+    
+def get_default_folder(path: Path,
+                       folder_name: str) -> Path:
+    """
+    Create a folder under the parent folder of 'training_results' with the name 'folder_name'.
+
+    Args:
+        path (Path): filepath of csv or a folderfile that contains results, e.g. predictions, history, etc.
+        folder_name (str): The name of the folder to be created.
+    Returns:
+        Path: The path to the newly created 'confusion_matrix' folder.
+    """
+
+    default_folder_path = None
+
+    if path.suffix == ".csv":
+        index_training_results = path.parts.index("training_results")
+        default_folder_path = Path(*path.parts[:index_training_results]) / folder_name
+    else:
+        for subpath in path.rglob("*"):  # rglob searches recursively for given pattern
+            if subpath.is_dir() and subpath.name == "training_results":
+                # Path found, get the parent of 'training_results'
+                parent_folder = subpath.parent
+                default_folder_path = parent_folder / folder_name
+                break
+    
+    if default_folder_path is None:
+        raise FileNotFoundError(f"Could not find 'training_results' folder in the path {path}.")
+    
+    return default_folder_path
+
+
+def get_newfilepath_from_predictions(predictions_filepath: Path,
+                                     suffix_name: str,
+                                     output_path: Optional[Path]):
+    """
+    Get the name of the confusion matrix file.
+
+    Args:
+        path (Path): The path of the confusion matrix file.
+
+    Returns:
+        string: The name of the confusion matrix file.
+    """
+    
+    file_name = predictions_filepath.name.replace("prediction_results",
+                                                  suffix_name)
+    if output_path is None:
+        folder_path = get_default_folder(predictions_filepath,
+                                         suffix_name)
+        print(colored(f"\nUsing default folder: {folder_path}.", 'magenta'))
+    else:
+        folder_path = output_path
+        
+    folder_path.mkdir(mode=0o777, parents=True, exist_ok=True)
+    
+    filepath = folder_path / file_name
+    
+    return filepath
+
+
+def get_filepath_from_results_path(results_path: Path,
+                                   folder_name: str,
+                                   file_name: str,
+                                   output_path: Optional[Path] = None) -> Path:
+
+    if output_path is None:
+        folder_path = get_default_folder(results_path,
+                                         folder_name)
+        print(colored(f"\nUsing default folder: {folder_path}.", 'magenta'))
+    else:
+        folder_path = output_path
+    
+    folder_path.mkdir(mode=0o777, parents=True, exist_ok=True)
+
+    filepath = folder_path / file_name
+    
+    return filepath
