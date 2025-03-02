8f90e01c7862888510e74f9421da3e8eccddf928
Paul Calle <pcallec@ou.edu>
2025-03-01 19:09:18 -0600

    Reorganize function into new file

---
+++ nachosv2/setup/utils_training.py
@@ -0,0 +1,17 @@
+def create_empty_history(is_cv_loop: bool,
+                         metrics_dictionary: dict):
+    """
+    TODO
+    """
+    
+    # Creates the base history
+    if is_cv_loop:
+        history = {'training_loss': [], 'validation_loss': [],
+                   'training_accuracy': [], 'validation_accuracy': []}
+    else:
+        history = {'training_loss': [], 'training_accuracy': []}
+    
+    # Adds the wanted metrics to the history dictionary
+    # add_lists_to_history(history, metrics_dictionary)
+
+    return history
