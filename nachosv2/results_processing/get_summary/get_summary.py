596ff5e6f93ddc54c93f42b7eab2838ea361df07
Paul Calle <pcallec@ou.edu>
2025-03-04 16:32:22 -0600

    adapt get_summary to work with CT and allow for other metrics

--- nachosv2/results_processing/get_summary/get_summary.py
+++ nachosv2/results_processing/get_summary/get_summary.py
@@ -13,60 +13,98 @@ from nachosv2.setup.utils import get_filepath_from_results_path
 from nachosv2.setup.files_check import ensure_path_exists
 from nachosv2.setup.get_config import get_config
 from nachosv2.setup.utils_processing import save_dict_to_yaml
-
-
-def parse_filename(filepath: Path,
-                   is_cv_loop: bool):
-    """
-    Parse parts from the filename to extract test fold, hyperparameter configuration, and validation fold.
-    """
-    parts = filepath.stem.split('_')
-    return {
-        'test_fold': parts[1],  # Assumes specific filename structure
-        'hp_config': parts[3],
-        'val_fold': parts[5] if is_cv_loop else None
-    }
+from nachosv2.setup.utils_processing import parse_filename
+from nachosv2.results_processing.get_metrics.get_metrics import generate_metrics_file
 
 
 def fill_dataframe(filepath: Path,
                    df: pd.DataFrame,
-                   is_cv_loop: bool):
+                   is_cv_loop: bool,
+                   output_path: Path,
+                   metrics_filepath: Path) -> pd.DataFrame:
     
     # Read CSV file into DataFrame
-    history_df = pd.read_csv(filepath)
+    history_df = pd.read_csv(filepath, index_col=0)
+    if metrics_filepath is not None:
+        metrics_df = pd.read_csv(metrics_filepath, index_col=0)
 
-    # Example: Extracting the epoch with the lowest loss (assuming a column 'loss' exists)
-    # You need to adjust this based on your actual CSV column names and what you define as 'best'
-    if 'validation_loss' in history_df.columns:
-        # Logic: it identifies the minimum value for 'validation loss'
-        best_val_loss_index = history_df['validation_loss'].idxmin()
-        best_val_loss_data = history_df.iloc[best_val_loss_index]
+    # if CV loop, 3 first columns are test, hp_config, and val
+    # if CT loop, 2 first columns are test, val    
+    index_col_metric = 3 if is_cv_loop else 2
+    l_metrics = metrics_df.columns[index_col_metric:]
+    
+    if is_cv_loop:
+        # Example: Extracting the epoch with the lowest loss (assuming a column 'loss' exists)
+        # You need to adjust this based on your actual CSV column names and what you define as 'best'
+        if 'validation_loss' in history_df.columns:
+            # Logic: it identifies the minimum value for 'validation loss'
+            best_val_loss_index = history_df['validation_loss'].idxmin()
+            best_val_loss_data = history_df.iloc[best_val_loss_index]
 
-         # Extract information from the file name
+            # Extract information from the file name
+            file_info = parse_filename(filepath, is_cv_loop)
+
+            dict_row = {
+                'test_fold': file_info['test_fold'],
+                'hp_config': file_info['hp_config'],
+                'val_fold': file_info['val_fold'],
+                # since the index is 0-based, we add 1 to get the actual epoch number
+                'n_epochs': [best_val_loss_index+1],
+                'training_loss': [best_val_loss_data['training_loss']],
+                'validation_loss': [best_val_loss_data['validation_loss']],
+                'training_accuracy': [best_val_loss_data['training_accuracy']],
+                'validation_accuracy': [best_val_loss_data['validation_accuracy']]
+            }
+            
+                     
+            for metric in l_metrics:
+                query = (
+                    "test_fold==@file_info['test_fold'] and "
+                    "val_fold==@file_info['val_fold'] and "
+                    "hp_config==@file_info['hp_config']"
+                    )
+                value_metric = metrics_df.query(query)[metric]
+                difference = best_val_loss_data['validation_accuracy'] - value_metric
+                if metric == "accuracy":
+                    if difference > 0.02:
+                        raise ValueError("Validation accuracy difference is greater than 2% for history and predictions")
+                else:
+                    dict_row[f"validation_{metric}"] = metrics_df.query(query)[metric]            
+                
+            # Prepare row as a DataFrame to be concatenated
+            new_row_df = pd.DataFrame(dict_row)
+
+            # Concatenate the new row to the existing DataFrame
+            df = pd.concat([df, new_row_df], ignore_index=True)
+    else:
+                # Extract information from the file name
         file_info = parse_filename(filepath, is_cv_loop)
+        
 
         # Prepare row as a DataFrame to be concatenated
-        new_row_df = pd.DataFrame({
+        dict_row = {
             'test_fold': file_info['test_fold'],
             'hp_config': file_info['hp_config'],
-            'val_fold': file_info['val_fold'],
             # since the index is 0-based, we add 1 to get the actual epoch number
-            'n_epochs': [best_val_loss_index+1],
-            'training_loss': [best_val_loss_data['training_loss']],
-            'validation_loss': [best_val_loss_data['validation_loss']],
-            'training_accuracy': [best_val_loss_data['training_accuracy']],
-            'validation_accuracy': [best_val_loss_data['validation_accuracy']]
-        })
-
-        # Concatenate the new row to the existing DataFrame
+            'n_epochs': [history_df.iloc[-1].name+1], # it extracts 0-based indexed of last row
+            'training_loss': [history_df.iloc[-1]['training_loss']],
+            'training_accuracy': [history_df.iloc[-1]['training_accuracy']],
+        }
+        for metric in l_metrics:
+            query = "test_fold==@file_info['test_fold'] and hp_config==@file_info['hp_config']"
+            dict_row[f"test_{metric}"] = metrics_df.query(query)[metric]
+        
+               
+        new_row_df = pd.DataFrame(dict_row)
         df = pd.concat([df, new_row_df], ignore_index=True)
-
+        
     return df
     
 
 def generate_summary(results_path: Path,
                      output_path: Optional[Path],
-                     is_cv_loop: Optional[bool]) -> Path:
+                     is_cv_loop: Optional[bool],
+                     metrics_filepath: Optional[Path]) -> Path:
     
     history_filepath_list = get_filepath_list(results_path,
                                               "history",
@@ -75,12 +113,17 @@ def generate_summary(results_path: Path,
     df_results = pd.DataFrame()
     
     # Extract and print test and validation fold numbers
-    for history_path in history_filepath_list:           
-        df_results = fill_dataframe(history_path, df_results, is_cv_loop)
+    for history_path in history_filepath_list:
+        df_results = fill_dataframe(history_path,
+                                    df_results,
+                                    is_cv_loop,
+                                    output_path,
+                                    metrics_filepath)
     
     summary_filepath = get_filepath_from_results_path(results_path=results_path,
                                                       folder_name="summary_analysis",
                                                       file_name="results_all.csv",
+                                                      is_cv_loop=is_cv_loop,
                                                       output_path=output_path)
 
     df_results.to_csv(summary_filepath)
@@ -90,8 +133,7 @@ def generate_summary(results_path: Path,
 
 def generate_avg_summary(summary_filepath: Path,
                           results_path: Path,
-                          output_path: Optional[Path],
-                          is_cv_loop: Optional[bool]) -> Path:
+                          output_path: Optional[Path]) -> Path:
     
     data = pd.read_csv(summary_filepath, index_col=0)  # Change to the path of your CSV file
 
@@ -112,6 +154,7 @@ def generate_avg_summary(summary_filepath: Path,
     average_filepath = get_filepath_from_results_path(results_path=results_path,
                                                       folder_name="summary_analysis",
                                                       file_name="avg_for_test.csv",
+                                                      is_cv_loop=True,
                                                       output_path=output_path)
 
     grouped_data.to_csv(average_filepath)
@@ -133,6 +176,7 @@ def get_best_hp_for_test(avg_filepath: Path,
     best_filepath = get_filepath_from_results_path(results_path=results_path,
                                                       folder_name="best_analysis",
                                                       file_name="best_hp_for_test.csv",
+                                                      is_cv_loop = True,
                                                       output_path=output_path)
 
     best_config_by_fold.to_csv(best_filepath)
@@ -193,23 +237,39 @@ def main(config = None):
     args = parse_command_line_args()
     # Defines the arguments
     config_dict = get_config(args['file'])
+    
     results_path = Path(config_dict['results_path'])
     ensure_path_exists(results_path)
+    
     output_path = config_dict.get('output_path', None)
     if output_path is not None:
         output_path = Path(output_path) 
     
     is_cv_loop = config_dict.get('is_cv_loop', None)
     
-    summary_filepath = generate_summary(results_path, output_path,
-                                        is_cv_loop)
+    list_metrics = config_dict.get('list_metrics', None)
+    if list_metrics == None:
+        list_metrics = ["accuracy"]
+    elif not isinstance(list_metrics, list):
+        list_metrics = [list_metrics]
     
-    avg_summary_filepath = generate_avg_summary(summary_filepath,
-                                                results_path,
-                                                output_path,
-                                                is_cv_loop)
+    metrics_filepath = generate_metrics_file(
+        list_metrics=list_metrics,
+        results_path=results_path,
+        output_path=output_path,
+        is_cv_loop=is_cv_loop)
+    
+    summary_filepath = generate_summary(results_path,
+                                        output_path,
+                                        is_cv_loop,
+                                        metrics_filepath)
     
     if is_cv_loop:    
+        
+        avg_summary_filepath = generate_avg_summary(summary_filepath,
+                                                    results_path,
+                                                    output_path)
+        
         configuration_cv_path = config_dict.get('configuration_cv_path', None)
         if configuration_cv_path is None:
             raise ValueError("Configuration path('configuration_path') must be provided for cross-validation loop.")
