e6e5e1b7271f590655c3679e45bfe0bdd77275b4
Paul Calle <pcallec@outlook.com>
2025-03-28 17:43:18 -0500

    use NACHOSv2_train in multiple

--- bash/run_multiple.sh
+++ bash/run_multiple.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /home/pcallec/anaconda3/etc/profile.d/conda.sh
 conda activate nachosv2
-python /home/pcallec/NACHOS/nachosv2/training/training_sequential/training_sequential.py --loop "cross-validation" --device "cuda:0" --file /home/pcallec/NACHOS/nachosv2/training/training_sequential/config_files/OCT_small/oct_small_1.yml --parallel
+NACHOSv2_train --loop "cross-validation" --device "cuda:0" --file /home/pcallec/NACHOS/nachosv2/training/training_sequential/config_files/OCT_small/oct_small_1.yml --parallel
