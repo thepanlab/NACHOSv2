f031a1bf3d0f69f30e51a45a613d82a8adca4039
Paul Calle <pcallec@ou.edu>
2025-05-27 15:48:12 -0500

    Modify write_timing_file to work with time modifications

--- nachosv2/modules/timer/write_timing_file.py
+++ nachosv2/modules/timer/write_timing_file.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from termcolor import colored
 from nachosv2.modules.timer.precision_timer import PrecisionTimer
 
+
 def write_timing_file(timer: PrecisionTimer,
                       directory_path: Path,
                       config_dict: dict,
@@ -15,20 +16,17 @@ def write_timing_file(timer: PrecisionTimer,
         is_verbose_on (bool): If the verbose mode is activated. Default is false. (Optional)
     """
     
-    if timer.is_timer_running():
-        raise Exception(colored(f"Error: timer not stopped", 'red'))
 
-    else:
-        # Initializations
-        elapsed_time = timer.get_elapsed_time()
-        timer_name = timer.get_timer_name()
-        
-        timing_file = f"{config_dict['job_name']}"+ \
-                      f"_TIME_SEQ_INNER_{timer_name}.txt"
-        timing_path = directory_path / timing_file
+    # Initializations
+    elapsed_time = timer.get_elapsed_time()
+    timer_name = timer.get_timer_name()
+    
+    timing_file = f"{config_dict['job_name']}"+ \
+                    f"_TIME_SEQ_INNER_{timer_name}.txt"
+    timing_path = directory_path / timing_file
 
-        with open(timing_path, 'w') as file_pointer:
-            file_pointer.write(str(elapsed_time))
+    with open(timing_path, 'w') as file_pointer:
+        file_pointer.write(str(elapsed_time))
 
-        if is_verbose_on: # If the verbose mode is activated
-            print(colored("Timing file succesfully created.", "cyan"))
+    if is_verbose_on: # If the verbose mode is activated
+        print(colored("Timing file succesfully created.", "cyan"))
